cf59dc042bbd46c25907d9890d173100b322aa1b
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-23 18:20:37 +0300

    Enable addition of transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -6,6 +6,7 @@ import AddTransactionForm from "./AddTransactionForm";
 function AccountContainer() {
   const apiHost = 'http://localhost:8001'
   const [transactions, setTransactions] = useState("")
+  const [newTransaction, setNewTransaction] = useState("")
 
   useEffect(() =>{
     fetch(`${apiHost}/transactions`)
@@ -13,10 +14,30 @@ function AccountContainer() {
     .then(data => setTransactions(data))
   }, [])
 
+  useEffect(()=>{
+    if(newTransaction){
+      fetch(`${apiHost}/transactions`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(newTransaction)
+      })
+      .then(result => result.json())
+      .then(data => setTransactions([...transactions, data]))
+    }
+    
+  },[newTransaction])
+
+  function handleAddTransactionForm(newTransaction){
+    setNewTransaction(newTransaction)
+  }
+
   return (
     <div>
       <Search />
-      <AddTransactionForm />
+      <AddTransactionForm handleAddTransaction={handleAddTransactionForm}/>
       <TransactionsList transactions={transactions}/>
     </div>
   );
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({handleAddTransaction}) {
+  const [formData, setFormData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: ""
+  })
+
+  function updateFormData(event){
+    setFormData({...formData, [event.target.name]: event.target.value})
+    console.log(formData)
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    handleAddTransaction(formData)
+  }
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" onChange={updateFormData} value={formData.date}/>
+          <input type="text" name="description" placeholder="Description" onChange={updateFormData} value={formData.description}/>
+          <input type="text" name="category" placeholder="Category" onChange={updateFormData} value={formData.category}/>
+          <input type="number" name="amount" placeholder="Amount" step="0.01" onChange={updateFormData} value={formData.amount}/>
         </div>
         <button className="ui button" type="submit">
           Add Transaction
